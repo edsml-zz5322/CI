d09e0094e920916f238e887d3e77aa955a5898dd
edsml-zz5322 <zhiyu.zhang22@imperial.ac.uk>
2023-03-21 23:17:30 +0000

    test

---
+++ main_test.cpp
@@ -0,0 +1,17 @@
+#include <cassert>
+#include "math.h"
+
+int main() {
+    // Test add function
+    assert(add(2, 3) == 5);
+    assert(add(-2, 3) == 1);
+    assert(add(0, 0) == 0);
+
+    // Test subtract function
+    assert(subtract(5, 3) == 2);
+    assert(subtract(5, 5) == 0);
+    assert(subtract(0, 0) == 0);
+
+    return 0;
+}
+
